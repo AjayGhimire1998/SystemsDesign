4302225afcc973e30d41ce018ed01b0daf0f72f1
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-08-08 08:27:54 +1000

    singleton and null object design pattern

--- src/com/ajayghimire/DesignPatterns/BehavioralPatterns/NullObjectDesignPattern/Cat.java
+++ src/com/ajayghimire/DesignPatterns/BehavioralPatterns/NullObjectDesignPattern/Cat.java
@@ -1,10 +1,15 @@
 package com.ajayghimire.DesignPatterns.BehavioralPatterns.NullObjectDesignPattern;
 
 public class Cat implements Animal {
+  private static final Cat CAT_INSTANCE = new Cat();
 
   private Cat() {}
 
   public static final Cat getInstance() {
+
+    if (CAT_INSTANCE != null) {
+      return CAT_INSTANCE;
+    }
     return new Cat();
   }
 
--- src/com/ajayghimire/DesignPatterns/BehavioralPatterns/NullObjectDesignPattern/Dog.java
+++ src/com/ajayghimire/DesignPatterns/BehavioralPatterns/NullObjectDesignPattern/Dog.java
@@ -1,11 +1,17 @@
 package com.ajayghimire.DesignPatterns.BehavioralPatterns.NullObjectDesignPattern;
 
 public class Dog implements Animal {
+  private static final Dog DOG_INSTANCE = new Dog();
+
   private Dog() {}
 
   public static final Dog getInstance() {
+    if (DOG_INSTANCE != null) {
+      return DOG_INSTANCE;
+    }
     return new Dog();
   }
+
   /** */
   @Override
   public void makeSound() {
