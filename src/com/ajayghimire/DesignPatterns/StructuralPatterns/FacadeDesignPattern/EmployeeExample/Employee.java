74e0b9bae3d92e3ccf8fb3a73b4d934ac1d04c09
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2024-08-09 19:31:46 +1000

    facade design

---
+++ src/com/ajayghimire/DesignPatterns/StructuralPatterns/FacadeDesignPattern/EmployeeExample/Employee.java
@@ -0,0 +1,9 @@
+package com.ajayghimire.DesignPatterns.StructuralPatterns.FacadeDesignPattern.EmployeeExample;
+
+public class Employee {
+  String emailID;
+
+  public Employee(String emailID) {
+    this.emailID = emailID;
+  }
+}
---
+++ src/com/ajayghimire/DesignPatterns/StructuralPatterns/FacadeDesignPattern/EmployeeExample/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.ajayghimire.DesignPatterns.StructuralPatterns.FacadeDesignPattern.EmployeeExample;
+
+public class EmployeeDAO {
+  void insert(){
+
+  }
+
+  void updateEmployee(){
+
+  }
+
+  Employee getEmployeeDetails(String emailID){
+
+  }
+}
